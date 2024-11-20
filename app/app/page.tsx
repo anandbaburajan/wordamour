5244e8d0d2e7bcb02ab3b77fe892c9cfb7be407c
anandbaburajan <anandbaburajan@gmail.com>
2024-11-20 17:44:20 +0530

    hmm

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -30,9 +30,10 @@ import WordSearch from "@blex41/word-search";
 import { PopoverClose } from "@radix-ui/react-popover";
 import { format } from "date-fns";
 import dayjs from "dayjs";
-import { ArrowRight, Eye, EyeOff, Printer, Shuffle } from "lucide-react";
+import { Eye, EyeOff, Printer, Shuffle } from "lucide-react";
 import { Instrument_Serif } from "next/font/google";
 import { useRef, useState } from "react";
+import generatePDF, { Margin, Resolution } from "react-to-pdf";
 import { toast } from "sonner";
 
 const instrumentSerif = Instrument_Serif({
@@ -66,10 +67,50 @@ export default function Home() {
 
   const popOverRef = useRef<HTMLButtonElement | null>(null);
 
+  const options = {
+    // default is `save`
+    method: "save",
+    filename: "wordamour.pdf",
+    // default is Resolution.MEDIUM = 3, which should be enough, higher values
+    // increases the image quality but also the size of the PDF, so be careful
+    // using values higher than 10 when having multiple pages generated, it
+    // might cause the page to crash or hang.
+    resolution: Resolution.MEDIUM,
+    page: {
+      // margin is in MM, default is Margin.NONE = 0
+      margin: Margin.SMALL,
+      // default is 'A4'
+      format: "A4",
+      // default is 'portrait'
+      orientation: "portrait",
+    },
+    canvas: {
+      // default is 'image/jpeg' for better size performance
+      mimeType: "image/jpeg",
+      qualityRatio: 1,
+    },
+    // Customize any value passed to the jsPDF instance and html2canvas
+    // function. You probably will not need this and things can break,
+    // so use with caution.
+    overrides: {
+      // see https://artskydj.github.io/jsPDF/docs/jsPDF.html for more options
+      pdf: {
+        compress: true,
+      },
+      // see https://html2canvas.hertzen.com/configuration for more options
+      canvas: {
+        useCORS: true,
+      },
+    },
+  };
+
+  // you can use a function to return the target element besides using React refs
+  const getTargetElement = () => document.getElementById("content-id");
+
   return (
-    <div className="h-screen font-[family-name:var(--font-geist-sans)]">
+    <div className="h-screen overflow-y-hidden font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-row h-full">
-        <div className="flex flex-col justify-between w-[23rem] border-gray-200 relative">
+        <div className="flex flex-col justify-between w-[20.6rem] border-gray-200 relative">
           <MeshGradientBackground />
           <div className="flex flex-col p-10 h-full w-full absolute">
             <Input
@@ -91,7 +132,7 @@ export default function Home() {
                   )}
                 </Button>
               </PopoverTrigger>
-              <PopoverContent className="w-auto p-0" align="start">
+              <PopoverContent className="w-fit p-0" align="start">
                 <Calendar
                   mode="single"
                   selected={puzzleDate}
@@ -126,7 +167,7 @@ export default function Home() {
               placeholder="Words (one per line)"
             />
             <Button
-              className="mb-10 bg-[#008A00] h-4 w-4 p-4 shadow-none text-white"
+              className="mb-10 bg-[#008A00] rounded-lg h-4 w-4 p-4 shadow-none text-white"
               onClick={() => {
                 if (wordsList.split(/\r?\n/).length > 32) {
                   toast.info("Only 32 words are allowed.");
@@ -159,10 +200,6 @@ export default function Home() {
               <SheetTrigger>
                 <span className="font-medium text-sm text-black/40 cursor-pointer h-fit flex items-center w-full justify-between underline">
                   See example questions for inspiration
-                  <ArrowRight
-                    className="ml-2 h-4 w-4 transition duration-200 group-hover:-rotate-45"
-                    strokeWidth={2}
-                  />
                 </span>
               </SheetTrigger>
               <SheetContent
@@ -179,8 +216,10 @@ export default function Home() {
                       <li>5+ illustrations</li>
                       <li>Choose between A4/A5 paper sizes</li>
                       <li>High-quality PDF download</li>
-                      <li>100% secure (your data is never stored with us)</li>
-                      <li>100% free (you can if you like!)</li>
+                      <li>
+                        What's your son's and/or daughter's name/nickname?
+                      </li>
+                      <li>What's your pet's name?</li>
                     </ul>
                   </SheetDescription>
                 </SheetHeader>
@@ -188,75 +227,152 @@ export default function Home() {
             </Sheet>
           </div>
         </div>
-        <div className="flex flex-col p-10 w-[calc(100vw-23rem)] bg-white relative">
-          <div className="absolute right-10 flex flex-col space-y-2">
-            <Button
-              onClick={() => {}}
-              className={`${
-                wordSearchObj ? "" : "invisible"
-              } items-center transition duration-200 bg-gradient-to-r from-[#ff5858] to-[#f09819] hover:from-black hover:to-black h-4 w-4 p-4 rounded-lg font-medium shadow-none text-white`}
-            >
-              <Printer className="h-3 w-3" strokeWidth={2} />
-            </Button>
-            <Button
-              className={`${
-                wordSearchObj ? "" : "invisible"
-              } items-center bg-gray-600/10 h-4 w-4 p-4 rounded-lg hover:bg-gray-600/20 font-medium transition duration-200 shadow-none text-gray-700`}
-              onClick={() => {
-                setUniqueCoords(getUniqueCoordinates(wordSearchObj.words));
-                setWordsShown(!wordsShown);
-              }}
-            >
-              {!wordsShown && (
-                <>
-                  <Eye className="h-3 w-3" strokeWidth={2} />
-                </>
-              )}
-              {wordsShown && (
-                <>
-                  <EyeOff className="h-5 w-5" strokeWidth={2} />
-                </>
-              )}
-            </Button>
+        <div className="flex flex-col w-[calc(100vw-20.6rem)] bg-white">
+          <div className="flex flex-col h-screen p-10 relative">
+            <div className="absolute right-10 flex flex-col space-y-2">
+              <Button
+                onClick={() => generatePDF(getTargetElement, options)}
+                className={`${
+                  wordSearchObj ? "" : "invisible"
+                } items-center transition duration-200 bg-gradient-to-r from-[#ff5858] to-[#f09819] hover:from-black hover:to-black h-4 w-4 p-4 rounded-lg font-medium shadow-none text-white`}
+              >
+                <Printer className="h-3 w-3" strokeWidth={2} />
+              </Button>
+              <Button
+                className={`${
+                  wordSearchObj ? "" : "invisible"
+                } items-center bg-gray-600/10 h-4 w-4 p-4 rounded-lg hover:bg-gray-600/20 font-medium transition duration-200 shadow-none text-gray-700`}
+                onClick={() => {
+                  setUniqueCoords(getUniqueCoordinates(wordSearchObj.words));
+                  setWordsShown(!wordsShown);
+                }}
+              >
+                {!wordsShown && (
+                  <>
+                    <Eye className="h-3 w-3" strokeWidth={2} />
+                  </>
+                )}
+                {wordsShown && (
+                  <>
+                    <EyeOff className="h-5 w-5" strokeWidth={2} />
+                  </>
+                )}
+              </Button>
+            </div>
+            <div className="flex justify-center w-full h-screen bg-white flex-col items-center">
+              <div className="flex justify-center mb-4">
+                <h1 className={`text-5xl ${instrumentSerif.className}`}>
+                  {title || "Untitled"}
+                </h1>
+              </div>
+              <div className="flex justify-center">
+                <h6 className="text-sm mb-16 text-gray-400">
+                  {dayjs(puzzleDate).format("MMM DD, YYYY")}
+                </h6>
+              </div>
+              <div className="flex justify-center">
+                <table className="text-sm">
+                  <tbody>
+                    {wordSearchObj &&
+                      wordSearchObj.grid.length > 0 &&
+                      wordSearchObj.grid.map((row, rowIndex) => (
+                        <tr key={rowIndex}>
+                          {row.map((cell, cellIndex) => (
+                            <td
+                              key={cellIndex}
+                              className={`${
+                                wordsShown &&
+                                uniqueCoords.some(
+                                  (coord) =>
+                                    coord.x === cellIndex &&
+                                    coord.y === rowIndex
+                                )
+                                  ? "bg-lime-300/50"
+                                  : ""
+                              } p-2 text-gray-900`}
+                            >
+                              {cell}
+                            </td>
+                          ))}
+                        </tr>
+                      ))}
+                  </tbody>
+                </table>
+              </div>
+            </div>
           </div>
-          <div className="flex justify-center w-full h-full flex-col items-center">
-            <div className="flex justify-center mb-4">
-              <h1 className={`text-5xl ${instrumentSerif.className}`}>
-                {title || "Untitled"}
-              </h1>
+          <div
+            className="flex flex-col p-10 relative w-[210mm] min-h-[594mm] justify-center items-center bg-white"
+            id="content-id"
+          >
+            <div className="flex justify-center w-full h-full bg-white flex-col items-center">
+              <div className="flex justify-center mb-8">
+                <h1 className={`text-5xl ${instrumentSerif.className}`}>
+                  {title || "Untitled"}
+                </h1>
+              </div>
+              <div className="flex justify-center">
+                <h6 className="text-sm mb-16 text-gray-400">
+                  {dayjs(puzzleDate).format("MMM DD, YYYY")}
+                </h6>
+              </div>
+              <div className="flex justify-center">
+                <table className="text-sm">
+                  <tbody>
+                    {wordSearchObj &&
+                      wordSearchObj.grid.length > 0 &&
+                      wordSearchObj.grid.map((row, rowIndex) => (
+                        <tr key={rowIndex}>
+                          {row.map((cell, cellIndex) => (
+                            <td key={cellIndex} className="p-2 text-gray-900">
+                              {cell}
+                            </td>
+                          ))}
+                        </tr>
+                      ))}
+                  </tbody>
+                </table>
+              </div>
             </div>
-            <div className="flex justify-center">
-              <h6 className="text-sm mb-12 text-gray-400">
-                {dayjs(puzzleDate).format("MMM DD, YYYY")}
-              </h6>
-            </div>
-            <div className="flex justify-center">
-              <table className="text-sm">
-                <tbody>
-                  {wordSearchObj &&
-                    wordSearchObj.grid.length > 0 &&
-                    wordSearchObj.grid.map((row, rowIndex) => (
-                      <tr key={rowIndex}>
-                        {row.map((cell, cellIndex) => (
-                          <td
-                            key={cellIndex}
-                            className={`${
-                              wordsShown &&
-                              uniqueCoords.some(
-                                (coord) =>
-                                  coord.x === cellIndex && coord.y === rowIndex
-                              )
-                                ? "bg-lime-300/50"
-                                : ""
-                            } p-2 text-gray-900`}
-                          >
-                            {cell}
-                          </td>
-                        ))}
-                      </tr>
-                    ))}
-                </tbody>
-              </table>
+            <div className="flex justify-center w-full h-full bg-white flex-col items-center">
+              <div className="flex justify-center mb-8">
+                <h1 className={`text-5xl ${instrumentSerif.className}`}>
+                  {title || "Untitled"}
+                </h1>
+              </div>
+              <div className="flex justify-center">
+                <h6 className="text-sm mb-16 text-gray-400">
+                  {dayjs(puzzleDate).format("MMM DD, YYYY")}
+                </h6>
+              </div>
+              <div className="flex justify-center">
+                <table className="text-sm">
+                  <tbody>
+                    {wordSearchObj &&
+                      wordSearchObj.grid.length > 0 &&
+                      wordSearchObj.grid.map((row, rowIndex) => (
+                        <tr key={rowIndex}>
+                          {row.map((cell, cellIndex) => (
+                            <td
+                              key={cellIndex}
+                              className={`${
+                                uniqueCoords.some(
+                                  (coord) =>
+                                    coord.x === cellIndex &&
+                                    coord.y === rowIndex
+                                )
+                                  ? "bg-lime-300/50"
+                                  : ""
+                              } p-2 text-gray-900`}
+                            >
+                              {cell}
+                            </td>
+                          ))}
+                        </tr>
+                      ))}
+                  </tbody>
+                </table>
+              </div>
             </div>
           </div>
         </div>
